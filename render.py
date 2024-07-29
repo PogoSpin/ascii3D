a60a89ab2360bb3de56d98deab1438416f8d1df7
Daniel Nyborg <daniel.sp.nyborg@gmail.com>
2024-07-30 02:51:37 +0400

    wip renderer, will do 2d drawing to the console

---
+++ render.py
@@ -0,0 +1,12 @@
+from lib import Vector2d
+
+class Rederer:
+    def __init__(self, windowSize: Vector2d, characterSet: list):
+        self.width = windowSize.x
+        self.height = windowSize.y
+
+        self.charSet = characterSet
+
+    #     self.
+
+    # def 
