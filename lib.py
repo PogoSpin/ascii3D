02b1ffe6044d844716de1f65ca22e952300e0225
Daniel Nyborg <daniel.sp.nyborg@gmail.com>
2024-07-29 15:54:51 +0400

    basic vector classes added

---
+++ lib.py
@@ -0,0 +1,10 @@
+class Vector3d:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+class Vector2d:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
