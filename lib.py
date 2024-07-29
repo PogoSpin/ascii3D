1883e028c086b53bbf0109ed23d3ce44e31d22d5
Daniel Nyborg <daniel.sp.nyborg@gmail.com>
2024-07-30 02:40:33 +0400

    added wip matrix class

--- lib.py
+++ lib.py
@@ -12,3 +12,35 @@ class Vector2d:
 class Position(Vector3d):
     def __init__(self, x, y, z):
         super().__init__(x, y, z)
+
+class Matrix:
+    def __init__(self, size: Vector2d, values: list = None):
+        self.width = size.x
+        self.height = size.y
+
+        self.values = []
+
+        if not values:
+            self.initializeEmptyValues()
+        
+
+    def initializeEmptyValues(self):
+            for _ in range(self.width * self.height):
+                self.values.append(None)
+
+    def get(self, row: int = None, column: int = None, pos: Vector2d = None):
+        if pos:
+            row = pos.x
+            column = pos.y
+
+        return self.values[row + (self.width * column)]
+    
+    def set(self, value, row: int = None, column: int = None, pos: Vector2d = None):
+        if pos:
+            row = pos.x
+            column = pos.y
+
+        self.values[row + (self.width * column)] = value
+    
+    def getVectorFromIndex(self, n):
+        return Vector2d(n % self.width, n // self.width)
