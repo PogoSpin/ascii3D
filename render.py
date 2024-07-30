bf6526ad3756c84f9bd9886ed1cd301a9d6dec08
Daniel Nyborg <daniel.sp.nyborg@gmail.com>
2024-07-30 18:31:51 +0400

    general structure for Render class

--- render.py
+++ render.py
@@ -1,12 +1,25 @@
-from lib import Vector2d
+from lib import Vector2d, Screen
 
 class Rederer:
-    def __init__(self, windowSize: Vector2d, characterSet: list):
+    def __init__(self, windowSize: Vector2d):
         self.width = windowSize.x
         self.height = windowSize.y
 
-        self.charSet = characterSet
+        self.screen = Screen(windowSize)
 
-    #     self.
+        self.draw = self.Draw(self.screen)
 
-    # def 
+    def display(self):
+        # print to screen
+        pass
+
+    class Draw:
+        def __init__(self, screen: Screen):
+            self.screen = screen
+
+        def point(self, pos: Vector2d):
+            self.screen.get(pos = pos).setBrightness(1)
+
+        def line(self, pos1: Vector2d, pos2: Vector2d):
+            # algorithm to draw line
+            pass
