2759711268fbc7cdd69743236839ce282dacf2ea
Daniel Nyborg <daniel.sp.nyborg@gmail.com>
2024-08-01 01:46:36 +0400

    added repr for vector3d class and __add__, __sub__

--- objects3d.py
+++ objects3d.py
@@ -4,6 +4,21 @@ class Vector3d:
         self.y = y
         self.z = z
 
+    def __add__(self, other):
+        if isinstance(other, Vector3d):
+            return Vector3d(self.x + other.x, self.y + other.y, self.z + other.z)
+        return NotImplemented
+        
+    def __sub__(self, other):
+        if isinstance(other, Vector3d):
+            return Vector3d(self.x - other.x, self.y - other.y, self.z - other.z)
+        return NotImplemented
+    
+    def __repr__(self) -> str:
+        return f'({self.x}, {self.y}, {self.z})'
+    
+
+
 class Position(Vector3d):
     def __init__(self, x: float, y: float, z: float):
         super().__init__(x, y, z)
