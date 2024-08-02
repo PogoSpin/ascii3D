dcebbd786760916d7827dea5280e3831ed4c0b6e
Daniel Nyborg <daniel.sp.nyborg@gmail.com>
2024-08-03 01:43:40 +0400

    basic camera, world and graphics class structure

---
+++ caster.py
@@ -0,0 +1,49 @@
+from objects3d import Vector3d
+from objects2d import Vector2d
+from render import Renderer
+
+# camera fov
+# camera distance
+# screen size = 1
+
+class Camera:
+    def __init__(self, position: Vector3d, orientation: Vector3d, fov: float):
+        self.position = position
+        self.orientation = orientation
+        
+        self.fov = fov
+
+class World:
+    def __init__(self, objects: set = set()):
+        self.objects = objects
+        self.beckGroundBrightness = 0
+
+        # other atributes of the world
+
+class Graphics:
+    def __init__(self, world: World, renderer: Renderer):
+        self.world = world
+        self.renderer = renderer
+
+    def renderWorld(self):
+        for screenspaceObjectPositions in self.projectWorld():
+            for point in screenspaceObjectPositions:
+                self.renderer.draw.point(point)
+
+    def projectWorld(self) -> list[list]:
+        projectedObjects = []
+        for obj in self.world.objects:
+            objectProjectedPositions = []
+
+            for vertexPos in obj.verticies:
+                screenspacePosition = self.projectToScreenspace(vertexPos)
+                objectProjectedPositions.append(screenspacePosition)
+            
+            projectedObjects.append(objectProjectedPositions)
+
+        return projectedObjects
+
+    def projectToScreenspace(self, position: Vector3d) -> Vector2d:
+        # code to project 3d position to 2d screenspace point
+        # for now, just passing through 3d position and ignoring z component
+        return Vector2d(position.x, position.y)
