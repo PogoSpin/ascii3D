19fd1722d9d1a561bfeca851b034b793b734918a
Daniel Nyborg <daniel.sp.nyborg@gmail.com>
2024-08-07 03:23:17 +0400

    actually working 3d to 2d projection & norm-screen
    added 3d to 2d projection with formulas I came up with
    that technique uses normalized screenspace of x and y being -1 to 1, with (0, 0) at center
    so also added convertion from normalized screenspace to pixel screen dimentions

--- graphics.py
+++ graphics.py
@@ -1,6 +1,7 @@
 from objects3d import Vector3d
 from objects2d import Vector2d, Screen
 from render import Renderer
+from math import tanh, radians
 
 # camera fov
 # camera distance
@@ -27,10 +28,13 @@ class Graphics:
 
         self.renderer.changeBackgroundBrightness(self.world.backgroundBrightness)
 
+        self.camera = Camera(Vector3d(0, 0, 0), None, radians(90))
+
     def renderWorld(self):
         for screenspaceObjectPositions in self.projectWorld():
             for point in screenspaceObjectPositions:
-                self.renderer.draw.point(point)
+                new = Vector2d(round((point.x + 1) * self.renderer.width / 2), round((point.y + 1) * self.renderer.height / 2))
+                self.renderer.draw.point(new)
 
     def projectWorld(self) -> list[list]:
         projectedObjects = []
@@ -48,4 +52,10 @@ class Graphics:
     def projectToScreenspace(self, position: Vector3d) -> Vector2d:
         # code to project 3d position to 2d screenspace point
         # for now, just passing through 3d position and ignoring z component
-        return Vector2d(position.x, position.y)
+        return Vector2d((2 * self.cameraAngleToPosX(position)) / self.camera.fov, (2 * self.cameraAngleToPosY(position)) / self.camera.fov)
+    
+    def cameraAngleToPosX(self, pointPos):
+        return tanh((pointPos.x - self.camera.position.x) / (pointPos.z - self.camera.position.z))
+    
+    def cameraAngleToPosY(self, pointPos):
+        return tanh((pointPos.y - self.camera.position.y) / (pointPos.z - self.camera.position.z))
